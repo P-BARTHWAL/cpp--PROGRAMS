0585caa9e6d5e1028bbc5f2babf042b37ea173c3
P-BARTHWAL <priyanshibarthwal06@gmail.com>
2026-08-15 12:30:35 +0530

    Adding arean file

---
+++ area_of_rectangle.cpp
@@ -0,0 +1,17 @@
+# include <iostream>
+using namespace std;
+int main() {
+  // Write length here
+   int length ;
+   cout << "Enter length: ";
+   cin >> length;
+  // Write width here
+   int width ;
+   cout << "Enter width: ";
+   cin >> width;
+  // Write area here (length * width)
+   int area = length * width; 
+  // Print area here
+  cout << "AREA OF RECTANGLE IS : " << area ;
+  return 0;
+}
